1c3c471c33119e3826701b384df69796ea1823f5
beilineili <1443036119@qq.com>
2023-12-07 16:20:24 +0800

    visual keylifecycle event.Message

--- internal/grafanadi/service/podphase.go
+++ internal/grafanadi/service/podphase.go
@@ -85,6 +85,8 @@ func ConvertPodPhase2Frame(podPhases []*storagemodel.LifePhase) model.DataFrame 
 				if msg != "" && msg != nil {
 					phaseMessage = msg.(string)
 				}
+				// 如果是eventObject则赋值message
+				op = msg.(string)
 			}
 			ext = phase.ExtraInfo.(map[string]interface{})["auditEvent.ResponseObject"]
 			if ext != nil {
